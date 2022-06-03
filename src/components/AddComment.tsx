83d89ab73d3584ac6a4c17306b9cb4e31ff76d8b
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-03 20:55:22 +0200

    Use CSS modules

--- src/components/AddComment.tsx
+++ src/components/AddComment.tsx
@@ -1,28 +1,32 @@
+import { useState } from 'react';
 import { InfoCircle } from 'tabler-icons-react';
-import '../css/AddComment.css';
+import styles from '../css/AddComment.module.css';
 import Avatar from '../static/Avatar1.jpg';
 
 function AddComment() {
+  const [text, setText] = useState<string>('');
   return (
-    <div className='addCommentContainer'>
-      <div className='addCommentBody'>
-        <h2 className='addCommentTitle'>Add a new comment</h2>
-        <img className='addCommentAvatar' src={Avatar} alt='sample avatar' />
+    <div className={styles.container}>
+      <div className={styles.body}>
+        <h2 className={styles.title}>Add a new comment</h2>
+        <img className={styles.avatar} src={Avatar} alt='sample avatar' />
         <textarea
           rows={4}
           placeholder='Type your message...'
-          className='addCommentInput'
+          className={styles.input}
+          value={text}
+          onChange={(e) => setText(e.currentTarget.value)}
         />
-        <div className='addCommentNote'>
-          <div className='addCommentTooltip'>
-            <InfoCircle className='addCommentTooltip' color='#bbb' />
-            <span className='addCommentTooltipText'>
+        <div className={styles.note}>
+          <div className={styles.tooltip}>
+            <InfoCircle color='#bbb' />
+            <span className={styles.tooltipText}>
               But please don't try to inject any JavaScript...
             </span>
           </div>
           <span>Some HTML is OK.</span>
         </div>
-        <button onClick={() => {}} className='addCommentButton'>
+        <button onClick={() => setText('')} className={styles.button}>
           Post Comment
         </button>
       </div>
--- src/components/ChitChat.tsx
+++ src/components/ChitChat.tsx
@@ -1,52 +1,52 @@
 import { useState } from 'react';
 import { Messages, MoodSmile, Paperclip } from 'tabler-icons-react';
-import '../css/ChitChat.css';
+import styles from '../css/ChitChat.module.css';
 import Avatar from '../static/Avatar1.jpg';
 
 function ChitChat() {
   const [text, setText] = useState<string>('');
   return (
-    <div className='chitChatContainer'>
-      <div className='chitChatBody'>
-        <div className='chitChatTop'>
-          <img src={Avatar} alt='demo avatar' className='chitChatAvatar' />
-          <span className='chitChatPartner'>You are speaking with Andreas</span>
+    <div className={styles.container}>
+      <div className={styles.body}>
+        <div className={styles.top}>
+          <img src={Avatar} alt='demo avatar' className={styles.avatar} />
+          <span className={styles.partner}>You are speaking with Andreas</span>
         </div>
-        <div className='chitChatMessages'>
-          <div className='chitChatToday'>Today</div>
-          <div className='chitChatMessageGroup'>
-            <div className='chitChatMyMessage'>
-              <div className='chitChatTimestamp'>11:14</div>
-              <div className='chitChatMyMessageText'>Good day!</div>
+        <div className={styles.messages}>
+          <div className={styles.today}>Today</div>
+          <div className={styles.messageGroup}>
+            <div className={styles.myMessage}>
+              <div className={styles.timestamp}>11:14</div>
+              <div className={styles.myMessageText}>Good day!</div>
             </div>
           </div>
-          <div className='chitChatMessageGroup'>
-            <div className='chitChatPartnerMessage'>
-              <div className='chitChatTimestamp'>11:15</div>
-              <div className='chitChatPartnerMessageText'>Hello!</div>
+          <div className={styles.messageGroup}>
+            <div className={styles.partnerMessage}>
+              <div className={styles.timestamp}>11:15</div>
+              <div className={styles.partnerMessageText}>Hello!</div>
             </div>
-            <div className='chitChatPartnerMessage'>
-              <div className='chitChatPartnerMessageText'>
+            <div className={styles.partnerMessage}>
+              <div className={styles.partnerMessageText}>
                 How can I assist you today?
               </div>
             </div>
           </div>
-          <div className='chitChatMessageGroup'>
-            <div className='chitChatMyMessage'>
-              <div className='chitChatTimestamp'>11:16</div>
-              <div className='chitChatMyMessageText'>
+          <div className={styles.messageGroup}>
+            <div className={styles.myMessage}>
+              <div className={styles.timestamp}>11:16</div>
+              <div className={styles.myMessageText}>
                 I need some help with the internet
               </div>
             </div>
-            <div className='chitChatMyMessage'>
-              <div className='chitChatMyMessageText'>It's not working</div>
+            <div className={styles.myMessage}>
+              <div className={styles.myMessageText}>It's not working</div>
             </div>
           </div>
-          <div className='chitChatToday'>Andreas is typing...</div>
+          <div className={styles.today}>Andreas is typing...</div>
         </div>
-        <div className='chitChatInput'>
+        <div className={styles.input}>
           <input
-            className='chitChatInputBox'
+            className={styles.inputBox}
             placeholder='Write a response...'
             value={text}
             onChange={(e) => setText(e.currentTarget.value)}
@@ -54,16 +54,16 @@ function ChitChat() {
               if (e.key === 'Enter') setText('');
             }}
           />
-          <div className='chitChatInputIcons'>
-            <MoodSmile className='chitChatInputIcon' />
+          <div className={styles.inputIcons}>
+            <MoodSmile className={styles.inputIcon} />
             <Paperclip
-              className='chitChatInputIcon'
+              className={styles.inputIcon}
               color='rgba(0, 0, 0, 0.2)'
             />
           </div>
         </div>
       </div>
-      <div className='chitChatFloatingIcon'>
+      <div className={styles.floatingIcon}>
         <Messages size={32} color='white' />
       </div>
     </div>
